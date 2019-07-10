3c8894d95e3c8940c4ec4599ba5c5c21bc94b251
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2019-07-10 18:12:08 +0300

    Working copy
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

--- plugins/task-plugin/src/machine/machines-picker.ts
+++ plugins/task-plugin/src/machine/machines-picker.ts
@@ -11,6 +11,7 @@
 import { injectable, inject } from 'inversify';
 import * as theia from '@theia/plugin';
 import { CheWorkspaceClient } from '../che-workspace-client';
+import { che as cheApi } from '@eclipse-che/api';
 
 const MACHINES_PLACE_HOLDER = 'Pick a machine to run the task';
 const RECIPE_CONTAINER_SOURCE = 'recipe';
@@ -22,84 +23,97 @@ export class MachinesPicker {
     @inject(CheWorkspaceClient)
     protected readonly cheWorkspaceClient!: CheWorkspaceClient;
 
-    private isOpen: boolean;
-    private hideToolContainers: boolean;
-
-    /**
-     * Returns a machine name if there's just one machine in the current workspace.
-     * Shows a quick open widget allows to pick a machine if there are several ones.
-     */
     async pick(): Promise<string> {
-
-        console.log('isOpen: ' + this.isOpen);
-        console.log('hideToolContainers: ' + this.hideToolContainers);
-
-        if (this.isOpen) {
-            // trigger show/hide tool containers
-            this.hideToolContainers = !this.hideToolContainers;
-        } else {
-            this.isOpen = true;
-            this.hideToolContainers = true;
-        }
-
-        console.log('isOpen after: ' + this.isOpen);
-        console.log('hideToolContainers after: ' + this.hideToolContainers);
-
-        const machines = await this.getMachines(this.hideToolContainers);
-        console.log('machines length: ' + machines.length);
-        console.log('machines: ' + machines);
-        if (machines.length === 1) {
-            return Promise.resolve(machines[0]);
-        }
-
-        const items: string[] = [];
-        for (const machineName of machines) {
-            items.push(machineName);
-        }
-
-        console.log('constructed items: ' + items);
-
-        return this.showMachineQuickPick(items);
+        return this.doPick(true);
     }
 
-    protected async getMachines(hideToolContainers: boolean): Promise<string[]> {
-        const machineNames: string[] = [];
-        const machines = await this.cheWorkspaceClient.getMachines();
-        console.log('machines from client: ' + machines);
-        if (!machines) {
-            return machineNames;
+    protected async doPick(hideToolingContainers: boolean): Promise<string> {
+        const containers = await this.getContainers(hideToolingContainers);
+
+        if (containers.length === 0) {
+            return Promise.reject(new Error('There isn\'t any container to execute a task.'));
         }
 
-        for (const machineName in machines) {
-            if (!machines.hasOwnProperty(machineName)) {
-                continue;
-            }
-            console.log('process: ' + machineName);
-
-            if (hideToolContainers) {
-                const machine = machines[machineName];
-                if (machine.attributes && (!machine.attributes[CONTAINER_SOURCE_ATTRIBUTE] || machine.attributes[CONTAINER_SOURCE_ATTRIBUTE] === RECIPE_CONTAINER_SOURCE)) {
-                    continue;
-                }
-            }
-
-            console.log('add into machines names: ' + machineName);
-
-            machineNames.push(machineName);
+        if (containers.length === 1) {
+            return Promise.resolve(containers[0]);
         }
 
-        return machineNames;
-    }
-
-    private showMachineQuickPick(items: string[]): Promise<string> {
         return new Promise<string>(resolve => {
 
+            const items: theia.QuickPickItem[] = [];
+            for (const machineName of containers) {
+                items.push(new ContainerItem(machineName));
+            }
+
+            if (hideToolingContainers) {
+                items.push(new LoadMoreItem());
+            }
+
             const options = { placeHolder: MACHINES_PLACE_HOLDER } as theia.QuickPickOptions;
-            options.onDidSelectItem = (item => {
-                const machineName = typeof item === 'string' ? item : item.label;
-                resolve(machineName);
+            options.onDidSelectItem = (async item => {
+                if (item instanceof ContainerItem) {
+                    resolve(item.label);
+                } else if (item instanceof LoadMoreItem) {
+                    const containerName = await this.doPick(false);
+                    resolve(containerName);
+                }
             });
             theia.window.showQuickPick(items, options);
         });
     }
+
+    protected async getContainers(hideToolContainers: boolean): Promise<string[]> {
+        const filteredContainers: string[] = [];
+        const containers = await this.cheWorkspaceClient.getMachines();
+        if (!containers) {
+            return filteredContainers;
+        }
+
+        for (const machineName in containers) {
+            if (!containers.hasOwnProperty(machineName)) {
+                continue;
+            }
+
+            if (hideToolContainers && !this.isToolingContainer(containers[machineName])) {
+                continue;
+            }
+
+            filteredContainers.push(machineName);
+        }
+
+        return filteredContainers;
+    }
+
+    private isToolingContainer(container: cheApi.workspace.Machine): boolean {
+        return container.attributes
+            && (!container.attributes[CONTAINER_SOURCE_ATTRIBUTE]
+                || container.attributes[CONTAINER_SOURCE_ATTRIBUTE] === RECIPE_CONTAINER_SOURCE);
+    }
+}
+
+export class ContainerItem implements theia.QuickPickItem {
+
+    constructor(
+        public readonly containerName: string,
+    ) {
+        this.label = containerName;
+    }
+
+    description: string;
+    detail: string;
+    label: string;
+    picked: boolean;
+}
+
+export class LoadMoreItem implements theia.QuickPickItem {
+
+    constructor() {
+        this.label = 'Show Tooling Containers...';
+    }
+
+    description: string;
+    detail: string;
+    label: string;
+    picked: boolean;
+
 }
