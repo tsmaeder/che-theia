f85e4c21166a92e185e1da4acad99758dac38bad
Thomas Mäder <tmader@redhat.com>
2021-02-16 12:29:51 +0000

    Generate .yarnrc to use a persistent link folder
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- linktheia.sh
+++ linktheia.sh
@@ -1,6 +1,18 @@
 BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 THEIA_LOCATION=/projects/theia
 
+if [ ! -f "/projects/.yarnrc" ]; then
+    echo "--link-folder /projects/links" > /projects/.yarnrc
+else 
+    echo .yarnrc already exists
+fi
+
+if [ ! -d "/projects/links" ]; then
+    mkdir /projects/links
+else 
+    echo links folder exists
+fi
+
 for r in packages dev-packages examples; do
     for d in ` ls $THEIA_LOCATION/$r/`; do
         directory=$THEIA_LOCATION/$r/$d
